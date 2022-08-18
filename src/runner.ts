162a7e72334ab8349503d471ae9aef063eb17509
taratalexander <taratalexander@gmail.com>
2022-08-18 19:45:50 +0400

    Implemented fetch function to get the AECA Debarred Statutory List of USA Directorate of Defense Trade Controls.

--- src/runner.ts
+++ src/runner.ts
@@ -12,6 +12,10 @@ if (argv.eu !== undefined) {
   console.log(`fetch eu list...`);
   require('../lib/fetch_eu').fetchEU();
 }
+if (argv.usa !== undefined) {
+  console.log(`fetch usa list...`);
+  require('../lib/fetch_usa').fetchUSA();
+}
 
 
 
